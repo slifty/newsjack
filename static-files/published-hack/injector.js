42b284a7a076965e72394384245239944890db50
Daniel Schultz <slifty@gmail.com>
2012-02-07 18:25:47 -0500

    Added a bar that fades in after 10 seconds announcing the newsjacking

--- static-files/published-hack/injector.js
+++ static-files/published-hack/injector.js
@@ -1,14 +1,19 @@
 (function() {
-  var topBarHTML = '';
+var topBarHTML = '<div class="hackpub-top-bar">' +
+	'<div class="newsjack-logo"><a href="http://www.newsjack.in"><img src="http://www.newsjack.in/logos/newsjack.jpg" /></a></div>' +
+	'<span class="desc">This page is a remix that created using <a href="http://www.newsjack.in" class="goggles">NewsJack</a></span>' +
+	'<div class="close-button"></div>' +
+	'</div>';
 
   function whenScriptsLoaded() {
-    $(document.head).append('<link rel="stylesheet" href="' +
+	$(document.head).append('<link rel="stylesheet" href="' +
                             path('top-bar.css') + '">');
     var topBar = $(topBarHTML);
+	topBar.hide();
     var original = $("a.original", topBar);
     original.attr("href", hackpubInfo.originalURL);
     original.text(original[0].hostname);
-    
+
     function removeTopBar() {
       topBar.fadeOut(function() { topBar.remove(); });
     }
@@ -18,6 +23,7 @@
       .click(removeTopBar);
     $(".close-button", topBar).click(removeTopBar);
     $(document.body).append(topBar);
+    setTimeout(function(){ topBar.fadeIn(15000); }, 5000);
   }
 
   function path(url) {
